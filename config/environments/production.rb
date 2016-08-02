8352cfd41a7bbc7b1adcd414dad7c89b6467dbd9
monikaCiv <monika.civic@gmail.com>
2016-08-02 15:24:32 -0400

    Reconfigure Action Mailer for Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'yourdomain.com',
+  :domain => 'https://pure-scrubland-36916.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
