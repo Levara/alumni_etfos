7833a432af169966b7408e6f44e861af502a767a
monikaCiv <monika.civic@gmail.com>
2016-08-02 14:47:15 -0400

    Change some routes 4

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,8 @@ Rails.application.routes.draw do
   get  'vijest/:id'=>  'static_pages#single_post'
  
   
-  #get 'contacts/new' => 'contacts#new',
-  #get 'contacts/create' => 'contact#create'
-
-  resources :contacts, only: [:new, :create], path: ''
+  get 'contacts/new' => 'contacts#new',         as: :contact_new
+  get 'contacts/create' => 'contacts#create',    as: :contact_create
 
   devise_for :users 
 
